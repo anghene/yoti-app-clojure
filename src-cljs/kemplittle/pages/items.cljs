46f00b8507b3703f9792497cf1543c81d94b3d47
Vlad Anghene <vlad.anghene@gmail.com>
2019-12-31 20:44:16 +0100

    tailwind to items component

--- src-cljs/kemplittle/pages/items.cljs
+++ src-cljs/kemplittle/pages/items.cljs
@@ -4,12 +4,13 @@
    [uix.dom.alpha :as uix.dom]
    [uix.core.alpha :as uix.core]
    [reitit.frontend.easy :as rfe]
+   [tailwind.core :refer [tw tw! spit-css!]]
    [reagent.core :as r]))
 
 (defn item-page [match]
   (let [{:keys [path query]} (:parameters match)
         {:keys [id]} path]
-    [:div
+    [:div {:class (tw! "w-1/2 m-auto p-6")}
      [:ul
       [:li [:a {:href (rfe/href :kemplittle.routes/item {:id 1})} "Item 1"]]
       [:li [:a {:href (rfe/href :kemplittle.routes/item {:id 2} {:foo "bar"})} "Item 2"]]]
